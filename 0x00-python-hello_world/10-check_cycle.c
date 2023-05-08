c4725c6fe4a7f25a4dab23ea0297d52b57e5748c
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-08 22:08:32 +0000

    done: task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Check if a single list has a cycle
+ * @list: Signle list
+ * Return: 1 if found or 0
+*/
+int check_cycle(listint_t *list)
+{
+listint_t *cpy = list;
+listint_t *tmp;
+while (cpy != NULL)
+{
+	tmp = cpy;
+	while (tmp->next != NULL)
+	{
+		if (tmp->next->n == cpy->n)
+			return (1);
+		tmp = tmp->next;
+	}
+	cpy = cpy->next;
+}
+return (0);
+}
