2671a02b6ec9ecaa79434894ee68c4f9233d75e5
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-10 18:38:46 +0000

    done: 100-print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for letter in list(range(97, 123).__reversed__()):
-    print(f"{chr(letter - 32)}" if letter % 2 else f"{chr(letter)}", end="")
+    print("{}".format(chr((letter - 32) if letter % 2 else letter)), end="")
