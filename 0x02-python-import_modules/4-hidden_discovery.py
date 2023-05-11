a62fe5f509b1a3501b7dfa820b489bcc4719c617
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-11 21:00:43 +0000

    done: 4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     _names = dir(hidden_4)
     for _name in _names:
-        if _name[0:2] != "__":
+        if _name[:2] != "__":
             print(_name)
