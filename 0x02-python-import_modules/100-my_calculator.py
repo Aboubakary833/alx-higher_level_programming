a2d022ac3c9821633352cb9502a5e295ddad2aaa
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-11 23:03:34 +0000

    done: 100-my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         '-': sub,
         '*': mul,
         '/': div
-	}
+        }
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     result = operators[operator](a, b)
     print("{} {} {} = {}".format(a, operator, b, result))
-       
