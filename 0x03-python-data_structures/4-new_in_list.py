248867def30cf1389519cd7b10136bb8350974f2
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-15 10:27:30 +0000

    done: 4-new_in_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,11 @@
 
 def new_in_list(my_list, idx, element):
     # Copy of the list
-    cpy = my_list[:]
-    if (idx < 0 or idx >= list(my_list)):
-        return cpy
-    else:
-        cpy.insert(idx, element)
-        cpy.pop(idx + 1)
-        return cpy
+    if (isinstance(my_list, list)):
+        cpy = my_list[:]
+        if (idx < 0 or idx >= len(my_list)):
+            return cpy
+        else:
+            cpy.insert(idx, element)
+            cpy.pop(idx + 1)
+            return cpy
