f93034df4a2d124d13ee6bbfae327512009ec8a1
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-15 22:22:02 +0000

    done: 100-print_python_list_info

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * is_palindrome - Check if a singly linked list is a palindrome
@@ -8,44 +7,18 @@
 */
 int is_palindrome(listint_t **head)
 {
-listint_t *current, *tmp, *reverse;
-int len = 0, i = 0, first[10240], last[10240];
-current = (*head);
-while (current != NULL)
-{
-	len++;
-	current = current->next;
-}
+listint_t *current, *reversed, *tmp;
+if ((*head) == NULL || (*head)->next == NULL)
+	return (1);
 current = (*head);
 tmp = (*head);
-reverse = reverse_listint(&tmp);
-if (len % 2 != 0)
+reversed = reverse_listint(&tmp);
+while (reversed != NULL && current != NULL)
 {
-while (i < len / 2 && current != NULL && reverse != NULL)
-{
-	first[i] = current->n;
-	last[i] = reverse->n;
-	current = current->next;
-	reverse = reverse->next;
-	i++;
-}
-}
-else
-{
-while (i < ((len / 2) + 1) && current != NULL && reverse != NULL)
-{
-	first[i] = current->n;
-	last[i] = reverse->n;
-	current = current->next;
-	reverse = reverse->next;
-	i++;
-}
-}
-
-for (; i >= 0; i--)
-{
-	if (first[i] != last[i])
+	if (reversed->n != current->n)
 		return (0);
+	reversed = reversed->next;
+	current = current->next;
 }
 return (1);
 }
