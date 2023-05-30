a8667c50aa738b3e1926e378207f8f955a524806
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-30 17:43:14 +0000

    done: 2-square done

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,9 @@ class Square:
         Args:
         size: The size of the new Square object
         """
-        if size <= 0:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size <= 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
