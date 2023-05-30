64b7888c27f429befe0c7aa7bd5bc2e6fd0dc6ce
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-05-30 17:36:19 +0000

    done: 1-square doen

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define an empty class Square."""
+
+
+class Square:
+    """Square class
+    """
+    def __init__(self, size):
+        """Initialize a new instance of Square
+
+        Args:
+        size: The size of the new Square object
+        """
+        self.__size = size
