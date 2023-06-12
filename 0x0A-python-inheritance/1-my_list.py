d9f3f73a2c34c2b0217b0cd26adebcf4e295389d
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-12 19:16:11 +0000

    done: my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -13,9 +13,4 @@ class MyList(list):
     def print_sorted(self):
         """print_sorted
         """
-        print('[', end="")
-        for i in range(len(self)):
-            if (i == len(self) - 1):
-                print("{:d}]".format(sorted(self)[i]))
-            else:
-                print("{:d}, ".format(sorted(self)[i]), end="")
+        print(sorted(self))
