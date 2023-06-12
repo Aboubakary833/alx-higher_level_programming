5bd5f4bfa55bba6d130a3fe2570c93b6b1a23c75
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-12 21:25:46 +0000

    done: Square

--- 0x0A-python-inheritance/9-rectange.py
+++ 0x0A-python-inheritance/9-rectange.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python3
 """Rectangle class that inherits from BaseGeometry class."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class BaseGeometry:
+    """BaseGeometry."""
+
+    def area(self):
+        """area
+
+                Raises:
+                        Exception: area() is not implemented
+                """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer_validator
+
+        Args:
+            name (str): Name.
+            value (int): Value.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
