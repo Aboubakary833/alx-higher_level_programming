ecc9f341a9fcf6038c9ba2425dbedc1ba6d75f1f
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-12 20:44:17 +0000

    done: my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,10 @@ class MyList(list):
     """MyList
     """
 
+    def __init__(self):
+        """Initialise the object"""
+        super().__init__()
+
     def print_sorted(self):
         """print_sorted
         """
