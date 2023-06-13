d97256c18eaffb451420ee1705be5c6401250f48
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-13 12:44:20 +0000

    done: write_file done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -11,4 +11,4 @@ def write_file(filename="", text=""):
             text (str, ""): The string to write. Defaults to "".
     """
     with open(filename, "w", encoding="utf-8") as file:
-        file.write(text)
+        return file.write(text)
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+"""Append at the end of a file"""
+
