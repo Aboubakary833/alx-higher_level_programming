e79d897caa732cf6a155226c828a3894dfe1d67e
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-13 13:28:43 +0000

    done: reading and parsing json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     Args:
             filename (str): Json file to read from
     """
-    with open(filename) as file:
-        return json.loads(file)
+    with open(filename, 'r') as file:
+        return json.load(file)
