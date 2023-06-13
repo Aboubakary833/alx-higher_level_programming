0ce90dc739fb0eb0ae03e002af8f0656b8938689
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-13 11:49:26 +0000

    read_file done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,11 +2,12 @@
 
 """Read file and print it content to stdout"""
 
-def read_file(filename=""):
-	"""read_file
 
-	Args:
-		filename (str, ""): File to read. Defaults to "".
-	"""
-	with open(filename, "r", encoding="utf-8") as file:
-		print(file.read())
+def read_file(filename=""):
+    """read_file
+
+    Args:
+            filename (str, ""): File to read. Defaults to "".
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
