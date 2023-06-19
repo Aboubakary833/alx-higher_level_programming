641629e86e4040c003e490235952f2cb4a3b8a5b
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-06-19 20:03:08 +0000

    started: rectangle test done

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,7 +6,8 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
-
+from io import StringIO
+from unittest.mock import patch
 
 class TestRectange(unittest.TestCase):
 
@@ -26,7 +27,7 @@ class TestRectange(unittest.TestCase):
         self.assertEqual(last_rectangle.height, 5)
         self.assertEqual(last_rectangle.x, 0)
         self.assertEqual(last_rectangle.y, 0)
-        self.assertEqual(last_rectangle.id, 4)
+        self.assertEqual(last_rectangle.id, 9)
 
     def test_less_argument(self):
         """ Test less number of args case"""
@@ -45,6 +46,26 @@ class TestRectange(unittest.TestCase):
         new_rectangle = Rectangle(5, 5)
         self.assertEqual(True, isinstance(new_rectangle, Base))
 
+    def test_display_case(self):
+        """ Test string printed """
+        r1 = Rectangle(2, 5)
+        res = "##\n##\n##\n##\n##\n"
+        with patch('sys.stdout', new=StringIO()) as str_out:
+            r1.display()
+            self.assertEqual(str_out.getvalue(), res)
+
+
+    def test_load_from_file_case2(self):
+        """ Test load JSON file """
+        first = Rectangle(5, 5)
+        second = Rectangle(8, 2, 5, 5)
+
+        linput = [first, second]
+        Rectangle.save_to_file(linput)
+        loutput = Rectangle.load_from_file()
+
+        for i in range(len(linput)):
+            self.assertEqual(linput[i].__str__(), loutput[i].__str__())
 
 if __name__ == '__main__':
     unittest.main()
