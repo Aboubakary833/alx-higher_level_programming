510ad0509c88c75e576d17877b96d0964d08301a
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-03-26 13:33:10 +0000

    chores: 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Create second_table table in a given database and insert values in it.
+CREATE TABLE IF NOT EXISTS second_table (
+  id int,
+  name varchar(256),
+  score int
+);
+
+INSERT INTO second_table (id, name,score) VALUES
+(1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+
