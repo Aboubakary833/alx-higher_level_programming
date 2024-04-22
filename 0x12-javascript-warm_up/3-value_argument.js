f91bba1e6fe8caec78d6c0fefa6095bbc7e9a913
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-04-22 11:39:33 +0000

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 
-const _l = process.argv.length;
-
-console.log(_l === 2 ? 'No argument' : process.argv[2]);
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : process.argv[2]);
